4c7aa83655f96f2bb3855692a13262d4bfd35e5b
rodri <rodri@DESKTOP-5V0QIBT>
2022-11-11 16:27:31 -0300

    restarted

---
+++ src/test/java/spring/projCrud/ProjCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.projCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
